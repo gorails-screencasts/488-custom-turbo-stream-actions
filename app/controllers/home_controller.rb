38a1a6114730c2f8393fe7129d84d6a2cc204a1c
Chris Oliver <excid3@gmail.com>
2023-04-03 08:07:57 -0500

    Add custom turbo stream actions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,13 @@ class HomeController < ApplicationController
   def index
   end
 
+  def example
+    respond_to do |format|
+      format.html
+      format.turbo_stream
+    end
+  end
+
   def terms
   end
 
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,26 +3,18 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import "@hotwired/turbo-rails"
-require("@rails/activestorage").start()
-//require("trix")
-//require("@rails/actiontext")
-require("local-time").start()
-require("@rails/ujs").start()
+import {StreamActions} from "@hotwired/turbo"
 
-import './channels/**/*_channel.js'
-import "./controllers"
+StreamActions.console_log = function() {
+  console.log(this.getAttribute("message"), this.templateContent, this.targetElements)
+}
 
-import * as bootstrap from "bootstrap"
-
-document.addEventListener("turbo:load", () => {
-  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
+StreamActions.notification = function() {
+  let title = this.getAttribute("title")
+  Notification.requestPermission(function(status) {
+    console.log(status)
+    if (status == "granted") {
+      new Notification(title)
+    }
   })
-
-  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-    return new bootstrap.Popover(popoverTriggerEl)
-  })
-})
+}
